045fc8ea11ec6ff66dc9aba6d6125f32181616cc
Anatoly Scherbakov <altaisoft@gmail.com>
2025-12-18 21:25:17 +0200

    ➕ Test for construct_flake8_args function

--- tests/test_flags.py
+++ tests/test_flags.py
@@ -1,4 +1,5 @@
 from jeeves_yeti_pyproject.flags import (
+    construct_flake8_args,
     construct_pytest_args,
 )
 from jeeves_yeti_pyproject.mypy import construct_mypy_flags
@@ -7,6 +8,10 @@ from jeeves_yeti_pyproject.mypy import construct_mypy_flags
 def test_pytest():
     assert list(construct_pytest_args(is_granular=True))
     assert list(construct_pytest_args(is_granular=False))
+
+
+def test_flake8():
+    assert list(construct_flake8_args())
 
 
 def test_mypy():
