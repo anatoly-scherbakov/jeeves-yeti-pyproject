1dbf49dec6ad521b77bc8c763b6cc94d76dd99fe
Anatoly Scherbakov <altaisoft@gmail.com>
2025-12-18 21:13:06 +0200

    🧹  test

--- tests/test_flags.py
+++ tests/test_flags.py
@@ -1,12 +1,7 @@
 from jeeves_yeti_pyproject.flags import (
-    construct_isort_args,
     construct_pytest_args,
 )
 from jeeves_yeti_pyproject.mypy import construct_mypy_flags
-
-
-def test_isort():
-    assert list(construct_isort_args())
 
 
 def test_pytest():
