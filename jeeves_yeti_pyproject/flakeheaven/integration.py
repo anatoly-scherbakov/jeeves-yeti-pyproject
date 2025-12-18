43b648e1ac233aa9fb27d18e55868adc31b2fc26
Anatoly Scherbakov <altaisoft@gmail.com>
2025-12-18 21:18:33 +0200

    Format flakeheaven/integration.py with ruff

--- jeeves_yeti_pyproject/flakeheaven/integration.py
+++ jeeves_yeti_pyproject/flakeheaven/integration.py
@@ -7,17 +7,17 @@ import typer
 from sh import ErrorReturnCode_1, poetry
 from sh.contrib import git
 
-logger = logging.getLogger('jeeves-yeti-pyproject:flakeheaven')
+logger = logging.getLogger("jeeves-yeti-pyproject:flakeheaven")
 
 
 def construct_config(project_directory: Path):
     """Construct transient TOML config for Flakeheaven."""
     return {
-        'tool': {
-            'flakeheaven': {
-                'base': [
-                    str(Path(__file__).parent / 'flakeheaven.toml'),
-                    str(project_directory / 'pyproject.toml'),
+        "tool": {
+            "flakeheaven": {
+                "base": [
+                    str(Path(__file__).parent / "flakeheaven.toml"),
+                    str(project_directory / "pyproject.toml"),
                 ],
             },
         },
@@ -27,7 +27,7 @@ def construct_config(project_directory: Path):
 def call(project_directory: Path):  # pragma: nocover
     """Execute flakeheaven against the project."""
     with tempfile.TemporaryDirectory() as directory:
-        config = Path(directory) / 'flakeheaven.toml'
+        config = Path(directory) / "flakeheaven.toml"
 
         config.write_text(
             config_content := tomlkit.dumps(
@@ -35,16 +35,16 @@ def call(project_directory: Path):  # pragma: nocover
             ),
         )
 
-        logger.debug('Transient config for flakeheaven: %s', config_content)
+        logger.debug("Transient config for flakeheaven: %s", config_content)
 
         try:
             poetry.run.flakeheaven.lint(
-                '--diff',
-                '--config',
+                "--diff",
+                "--config",
                 config,
-                '.',
+                ".",
                 _cwd=project_directory,
-                _in=git.diff('origin/master'),
+                _in=git.diff("origin/master"),
             )
         except ErrorReturnCode_1 as err:
             typer.echo(err.stdout)
